fab2116f1e7ba20f4fd930bef4083e29f42b1f7f
Ignacio Tolosa <87409874+iLToR0@users.noreply.github.com>
2023-10-29 21:21:49 -0300

    staticFiles

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,11 +23,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', default='your secret key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True #'RENDER' not in os.environ
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
-#ALLOWED_HOSTS = []
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
+
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 if not DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
---
+++ staticfiles/javascript/input_page.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let form = document.getElementById('backtesting-form');
+
+    form.addEventListener('submit', function (event) {
+        let fechaDesde = new Date(document.getElementById('id_fechaDesde').value + 'T00:00:00').getTime();
+        let fechaHasta = new Date(document.getElementById('id_fechaHasta').value + 'T00:00:00').getTime();
+        let valorTK = parseFloat(document.getElementById('id_valorTK').value);
+        let minDate = new Date('2023-05-01' + 'T00:00:00').getTime();
+        let maxDate = new Date('2023-09-30' + 'T00:00:00').getTime();
+        let processingMessage = document.getElementById('processing-message');
+
+        dataValidate(fechaDesde, fechaHasta, valorTK, minDate, maxDate, processingMessage);
+
+    });
+
+});
+
+function showError(message) {
+    let errorMessage = document.getElementById('error-message');
+    errorMessage.innerHTML = message;
+    errorMessage.style.display = 'block';
+}
+
+function dataValidate(fechaDesde, fechaHasta, valorTK, minDate, maxDate, processingMessage) {
+    if (fechaDesde === fechaHasta) {
+        event.preventDefault();
+        showError('Las fechas no pueden ser iguales. Por favor, cambie las fechas.');
+    } else if (fechaDesde> fechaHasta) {
+        event.preventDefault();
+        showError('La fecha de inicio no puede ser mayor que la fecha de fin. Por favor, selecciona fechas válidas.');
+    } else if (fechaDesde < minDate || fechaHasta > maxDate) {
+        event.preventDefault();
+        showError('No hay datos disponibles para las fechas ingresadas. Por favor, cambie las fechas.');
+    } else if (valorTK <= 0) {
+        event.preventDefault();
+        showError('El valor de valorTK no puede ser negativo. Por favor, ingresa un valor válido.');
+    } else {
+        processingMessage.style.display = 'flex';
+    }
+}
+
+
+
+
+
+
+
+
+
